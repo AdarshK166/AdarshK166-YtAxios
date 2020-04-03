a6b519670f812aba17270abc54bde865c7ba9122
AdarshK166 <kolvekaradarsh@gmail.com>
2020-04-03 10:49:25 +0530

    added cdn

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 function getTodos() {
   var stext = document.getElementById("ytSearch").value;
   axios
-    .get("https://www.googleapis.com/youtube/v3/search", {
-      params: {
-        key: "AIzaSyBOxtVp_Q9hRdHYDGjXcpzOgrzJ5hy3qyQ",
-        part: "snippet",
-        maxResults: 3,
-        q: stext,
-        order: "viewCount"
+    .get(
+      "https://www.googleapis.com/youtube/v3/search?key=AIzaSyBOxtVp_Q9hRdHYDGjXcpzOgrzJ5hy3qyQ",
+      {
+        params: {
+          part: "snippet",
+          maxResults: 3,
+          q: stext,
+          order: "viewCount"
+        }
       }
-    })
-    .then(res => console.log(res.data))
+    )
+    .then(res => showOutput(res))
     .catch(err => console.error(err));
 }
 
 function showOutput(res) {
+  var obj = JSON.parse(res);
+  console.log(obj.data);
+  /*
   document.getElementById("res").innerHTML = `
   <div>
-    <pre>Status: ${res.data}</pre>
+    <pre>Status: ${res.data.items}</pre>
   </div>
 `;
+*/
 }
