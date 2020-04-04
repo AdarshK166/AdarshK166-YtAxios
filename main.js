a2d6424d65bd3d3844da0d5f576bfb9507dd7806
AdarshK166 <kolvekaradarsh@gmail.com>
2020-04-04 21:04:29 +0530

    commit 4

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ function getTodos() {
           q: stext,
           type: "video",
           maxResults: 6,
-          order: "viewCount"
+          order: "relevance",
+          safeSearch: "moderate"
         }
       }
     )
@@ -19,14 +20,11 @@ function getTodos() {
 
 function showOutput(res) {
   window.res = res;
-  console.log(res);
   var stext = document.getElementById("ytSearch").value;
-  //console.log(res.data);
-  //console.log(res.data.items[i].snippet.title);
   document.getElementById("res1").innerHTML = "";
   for (var i = 0; i <= 5; i++) {
     document.getElementById("res1").innerHTML += `
-  <div id=${i} class="resultsDiv" onclick="openLink(id)">
+  <div id=${i} class="resultsDiv" onclick="openLink(id) style="cursor: pointer"">
   <p>
   <img class="thumbImg" style="vertical-align:middle" src="http://img.youtube.com/vi/${res.data.items[i].id.videoId}/sddefault.jpg" alt="alt image" width="100">
   <span>${res.data.items[i].snippet.title}</span>
@@ -39,27 +37,14 @@ function showOutput(res) {
 function openLink(j) {
   document.getElementById("res").innerHTML = "";
   document.getElementById("res").innerHTML = `
-    <div class="icont">
-    <iframe class="embed-responsive-item"
-    src="https://www.youtube.com/embed/${res.data.items[j].id.videoId}" width="1000px" height="500px"
+    <div class="vid-container">
+    <iframe class="frame-container"
+    src="https://www.youtube.com/embed/${res.data.items[j].id.videoId}"
     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen
     ></iframe>
+    <h1>${res.data.items[j].snippet.title}</h1>
+    <h5>${res.data.items[j].snippet.description}</h5>
     </div>
   `;
 }
-
-/*
-<div class="icont">
-  <div class="embed-responsive embed-responsive-21by9">
-  <iframe class="embed-responsive-item"
-  src="https://www.youtube.com/embed/${res.data.items[0].id.videoId}"
-  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-  allowfullscreen
-  ></iframe>
-  </div>
-  </div>
-
-
-<pre>Status: ${res.data.items[0].snippet.title}</pre>
-*/
